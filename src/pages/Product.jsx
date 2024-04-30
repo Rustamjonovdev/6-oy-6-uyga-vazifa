7b7ce602f63a696e13a5da471d36db3b7ff2851c
Rustamjonov <rustamjonovshahzodbek0@gamil.com>
2024-04-30 11:03:48 +0500

    product all data add

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,38 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 function Product() {
-    const {id} = useParams()
+  const { id, thumbnail } = useParams();
+  const {
+    data: product,
+    isPending,
+    error,
+  } = useFetch("https://dummyjson.com/products/" + id);
+  console.log(product);
   return (
-    <div className="align-content">
-    <h1 className="text-3xl">Product - {id}</h1>
-    </div>
-  )
+    <>
+      {product && (
+        <div className="align-content">
+          <img src={thumbnail} alt="" />
+          <h1 className="text-4xl mb-3 text-center"><b>Product - {product.title}</b></h1>
+          <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box mb-3">
+            {product.images.map((image) => {
+              return (
+                <div key={image} className="carousel-item">
+                <img src={image} className="rounded-box max-h-64 lg:max-h-96 h-full object-contain" />
+              </div>
+              )
+            })}
+          </div>
+          <p className="">{product.description}</p>
+          <h2 className="text-3xl"><b>Price:</b> ${product.price}</h2>
+          <p>Rating:  {product.rating}</p>
+          <p>Stock: {product.stock}</p>
+
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Product
+export default Product;
